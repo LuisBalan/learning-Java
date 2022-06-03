ca6245aa436dbe434f10fb11066808edc8e052f4
LuisBalan <jlbalcz@gmail.com>
2022-06-03 02:29:00 -0500

    organize folders and add 02 lesson

---
+++ Variables/01/Creator.java
@@ -0,0 +1,21 @@
+//typing of variables are declared
+
+/* Examples
+int age;
+double salaryRequirement
+boolean isEmployed
+*/
+
+/*
+Assignment
+age=85
+int yearCodecademyWasFounded = 2011;
+*/
+public class Creator{
+    public static void main(String[] args){
+        String name = "James Gosling";
+        int yearCreated = 1995;
+        System.out.println(name);
+        System.out.println(yearCreated);
+    }
+}
---
+++ Variables/02/CountComment.java
@@ -0,0 +1,9 @@
+
+public class CountComment {
+    public static void main(String[] args){
+        int numComments = 6;
+        System.out.println(numComments);
+
+    }
+    
+}
