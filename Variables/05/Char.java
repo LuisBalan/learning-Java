b86536087d1f4665970df2722b797d8c0933fabe
LuisBalan <jlbalcz@gmail.com>
2022-06-03 03:17:18 -0500

    add exercises about variables on Java

---
+++ Variables/05/Char.java
@@ -0,0 +1,8 @@
+//another type of data is char. it is for characters
+
+public class Char {
+    public static void main(String[] args){
+        char expectedGrade = 'A';
+        System.out.println(expectedGrade);
+    }    
+}
---
+++ Variables/MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile{
+    public static void main(String[] args) {
+        String name = "Luis";
+        int age = 31;
+        double desiredSalary = 19500.50;
+        char gender = 'm';
+        boolean lookingForJob = true;   
+    }
+}
