7a101610c1f6d0fec32a55aa5ae445cc3df99956
LuisBalan <jlbalcz@gmail.com>
2022-06-03 01:48:37 -0500

    initial comit

---
+++ Tree.java
@@ -0,0 +1,15 @@
+public class Tree{
+    public static void main(String[] args){
+        //The main task of this class is plant a tree.
+       
+        System.out.println("Hey there, I'm Luis and I'm learning to code in Java!");
+        System.out.println("I'm going to plant a tree today!");
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println("  ***  ");
+        System.out.println("   *   ");
+        System.out.println("   *   ");
+        System.out.println("#######");
+
+    }
+}
