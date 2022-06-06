1a6b7a3f80985e6c10c7f6608a30d334133e8d7a
LuisBalan <jlbalcz@gmail.com>
2022-06-06 14:41:53 -0500

    add instance of Store

--- Introduction_To_Classes/02/Store.java
+++ Introduction_To_Classes/02/Store.java
@@ -1,5 +1,13 @@
 public class Store{
-    public static void main(String[] args){
 
+    public Store(){
+        System.out.println("I am inside the constructor method.");
     }
+
+    public static void main(String[] args){
+        System.out.println("Start of the main method.");
+        Store lemonadeStand = new Store();
+        System.out.println(lemonadeStand);
+    }
+
 }
