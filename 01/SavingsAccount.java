4b05c79877e132d3839f0a3f8b7cc607af66baf2
LuisBalan <jlbalcz@gmail.com>
2022-06-07 16:18:20 -0500

    add first SavingsAccount class

---
+++ 01/SavingsAccount.java
@@ -0,0 +1,25 @@
+public class SavingsAccount{
+    int balance;
+
+    public SavingsAccount(int initialBalance){
+        balance = initialBalance;
+    }
+
+    public static void main(String[] args){
+        SavingsAccount savings = new SavingsAccount(2000);
+
+        //check balance:
+        System.out.println("Hello!");
+        System.out.println("Your balance is " + savings.balance);
+        
+        //Withdrawing:
+        int afterWithdraw = savings.balance - 300;
+        savings.balance = afterWithdraw;
+        System.out.println("You just withdrew " + 300);
+
+        //Check balance:
+
+    }
+
+
+}
