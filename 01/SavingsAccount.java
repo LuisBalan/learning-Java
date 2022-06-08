5577755aa6429880cb633f8d135022ecb724014e
LuisBalan <jlbalcz@gmail.com>
2022-06-08 01:43:57 -0500

    add Store class

--- 01/SavingsAccount.java
+++ 01/SavingsAccount.java
@@ -18,6 +18,17 @@ public class SavingsAccount{
         System.out.println("You just withdrew " + 300);
 
         //Check balance:
+        System.out.println("Hello!");
+        System.out.println("Your balance is " + savings.balance);
+
+        //Deposit:
+        int afterDeposit = savings.balance + 600;
+        savings.balance = afterDeposit;
+        System.out.println("You just deposite " + 600);
+
+        //Check balance:
+        System.out.println("Hello!");
+        System.out.println("Your balance is: " + savings.balance);
 
     }
 
