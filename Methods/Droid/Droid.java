392e2437f1e9c7f2512fed42e359aa226f08a488
LuisBalan <jlbalcz@gmail.com>
2022-06-14 21:19:40 -0500

    add instance fields and constructor

---
+++ Methods/Droid/Droid.java
@@ -0,0 +1,16 @@
+public class Droid{
+
+    String name;
+    int batteryLevel;
+
+    public Droid(String robotName, String initialBatteryLevel){
+        name = robotName;
+        batteryLevel = initialBatteryLevel;
+    }
+
+
+
+    public static void main(){
+
+    }
+}
