b2029153eaa00f1080d69688dd33263ec340968c
LuisBalan <jlbalcz@gmail.com>
2022-06-16 16:19:55 -0500

    add second exercise on conditional statements

---
+++ Conditionals_Control_Flow/02/Order.java
@@ -0,0 +1,11 @@
+public class Order{
+
+    public static void main(String[] args){
+        boolean isFilled = false;
+        if(isFilled == true){
+            System.out.println("Shipping");
+        }else{
+            System.out.println("Order not ready");
+        }
+    }
+}
