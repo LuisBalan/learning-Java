1b53dcb986a8a0962f5d3e0970fb84345b1d04de
LuisBalan <jlbalcz@gmail.com>
2022-06-17 20:54:10 -0500

    changes in exercise on logic operators

--- Conditional_Operators/05/Operators.java
+++ Conditional_Operators/05/Operators.java
@@ -5,12 +5,12 @@ public class Operators{
 
         boolean ex1 = !(a == 7 && (b >= a || a != a));
 
-        //System.out.println(ex1);
+        System.out.println(ex1);
 
         boolean ex2 = a == b || !(b > 3);
-        // System.out.println(ex2);
+        System.out.println(ex2);
 
         boolean ex3 = !(b <= a && b != a + b);
-        //System.out.println(ex3);
+        System.out.println(ex3);
     }
 }
